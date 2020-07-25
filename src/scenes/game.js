af92ecf6224fe767e67ce3ebb275b38319c1a84b
Joshua <joshua@psychotechnology.com>
2020-07-25 16:43:53 +0100

    Bootstrapped project with Phaser 3 Webpack Template

---
+++ src/scenes/game.js
@@ -0,0 +1,24 @@
+import logoImg from "../assets/logo.png";
+
+class playGame extends Phaser.Scene {
+    constructor () {
+        super('playGame');
+    }
+    preload() {
+        this.load.image("logo", logoImg);
+    }
+    create() {
+        const logo = this.add.image(400, 150, "logo");
+        
+        this.tweens.add({
+            targets: logo,
+            y: 450,
+            duration: 2000,
+            ease: "Power2",
+            yoyo: true,
+            loop: -1
+        });
+    }
+}
+
+export default playGame;
